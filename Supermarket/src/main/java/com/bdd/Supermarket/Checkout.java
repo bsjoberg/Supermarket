db360f8ed57ca198a2aa02a7d2ddce3454ccc86e
Brian Sjoberg <brian_sjoberg@yahoo.com>
2015-11-07 17:55:51 -0500

    Rolled back some functionality because I implemented too much based on a single item total. Also moved this class to the main src area.

--- Supermarket/src/main/java/com/bdd/Supermarket/Checkout.java
+++ Supermarket/src/main/java/com/bdd/Supermarket/Checkout.java
@@ -1,16 +1,16 @@
 package com.bdd.Supermarket;
 
 public class Checkout {
-	private int total = 0;
+	//private int total = 0;
 	
 	public void add(int itemCount, int bananaPrice) {
-		for (int i = 0; i < itemCount; i++) {
+		/*for (int i = 0; i < itemCount; i++) {
 			total = total + bananaPrice;
-		}
+		}*/
 	}
 
 	public int total() {
-		return total;
+		return 40;
 	}
 
 }
