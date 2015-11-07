7c3c65f7cbf467f17b9908448241f484a1fa1e08
Brian Sjoberg <brian_sjoberg@yahoo.com>
2015-11-07 17:39:05 -0500

    Initial checkout steps file.

--- Supermarket/src/test/java/com/bdd/Supermarket/SupermarketRunnerTest.java
+++ Supermarket/src/test/java/com/bdd/Supermarket/SupermarketRunnerTest.java
@@ -3,12 +3,14 @@ package com.bdd.Supermarket;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"pretty", "html:target/resources"} ,
-		features = "src/test/resources"
+		features = "src/test/resources",
+		snippets = SnippetType.CAMELCASE
 		)
 public class SupermarketRunnerTest {
 
